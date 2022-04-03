243b519759ec8b0fc91df43e613a0641ce8832e3
= <adedejifavour6@gmail.com>
2022-04-03 18:03:00 +0400

    nodemailer added

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,4 +1,17 @@
+const nodemailer = require("");
+
 const sendEmail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "glenda.macejkovic36@ethereal.email",
+      pass: "C5Gz1vBr1A7BfVK3TX",
+    },
+  });
+
   res.send("This is the email");
 };
 
